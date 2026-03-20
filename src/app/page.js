fe0378cbffd7bda3f5f351bd341277d905d96d01
Muhammad Kaif <kaiftanveer943@gmail.com>
2026-03-20 16:04:13 +0500

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client"
 
+import { useEffect } from "react";
 import AboutSection from "./components/AboutSection";
 import AskKaifSection from "./components/AskKaifSection";
 import ContactSection from "./components/ContactSection";
@@ -9,6 +10,13 @@ import ServicesSection from "./components/ServicesSection";
 import TechStack from "./components/TechStack";
 
 export default function Home() {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.history.scrollRestoration = "manual";
+      window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+    }
+  }, []);
+
   return (
     <main style={{ background: "#09090f", padding: "0" }}>
       <HeroSection />
